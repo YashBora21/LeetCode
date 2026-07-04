947b42697d3b51ac7b97117df40f2574d1299395
Yash Yogesh Bora <yashbora567@gmail.com>
2026-07-04 10:16:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (10.69%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -3,12 +3,15 @@ public:
     int fibonacci(int n,vector<int>&dp){
 
         if(n==1||n==0)return n;
-        dp[0]=0;
-        dp[1]=1;
+        int past2=0;
+        int past1=1;
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int curr=past1+past2;
+            past2=past1;
+            past1=curr;
+
         }
-        return dp[n];
+        return past1;
     }
     int fib(int n) {
         //your code goes here
