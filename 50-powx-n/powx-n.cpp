36be4d5b095454d8f92a5e0dc70cb0e852080b43
Yash Yogesh Bora <yashbora567@gmail.com>
2026-07-27 07:38:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.08%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long exp=n;
+        double ans=1;
+        if(exp<0){
+            x=1.0/x;
+            exp=-exp;
+
+        }
+        while(exp>0){
+            if(exp%2!=0){
+             ans*=x;
+            }
+            x*=x;
+            exp/=2;
+        }
+        return ans;
+    }
+};
